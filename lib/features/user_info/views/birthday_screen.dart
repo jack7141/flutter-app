a01d69f35932eae747eb6f03e64a213f9200c940
황광회 <ghl92479@gmail.com>
2025-06-19 15:46:34 +0900

    ADD: 생일 페이지 Init

--- lib/features/user_info/views/birthday_screen.dart
+++ lib/features/user_info/views/birthday_screen.dart
@@ -1,12 +1,69 @@
+import 'package:celeb_voice/constants/gaps.dart';
+import 'package:celeb_voice/constants/sizes.dart';
+import 'package:celeb_voice/features/user_info/views/mbti_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BirthdayScreen extends StatelessWidget {
   static const String routeName = "birthday";
 
   const BirthdayScreen({super.key});
 
+  void _onNextTap(BuildContext context) {
+    context.pushNamed(MbtiScreen.routeName);
+  }
+
+  void _onPressIconButton(BuildContext context) {
+    context.pop();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+      appBar: AppBar(
+        titleSpacing: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, size: 28),
+          onPressed: () => _onPressIconButton(context),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.size20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 32,
+                    backgroundImage: NetworkImage(
+                      "https://avatars.githubusercontent.com/u/3612017",
+                    ),
+                  ),
+                  Gaps.h12,
+                  Text(
+                    '성시경',
+                    style: TextStyle(
+                      fontSize: Sizes.size16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Gaps.v20,
+              Text(
+                "생일은 언제에요?",
+                style: TextStyle(
+                  fontSize: Sizes.size16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
