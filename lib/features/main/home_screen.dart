9fcbcf8fdbb5c91f63f09b69a34d5e43a642758f
황광회 <ghl92479@gmail.com>
2025-06-24 16:23:46 +0900

    Update: Main UI 수정

--- lib/features/main/home_screen.dart
+++ lib/features/main/home_screen.dart
@@ -1,5 +1,8 @@
 // lib/features/main/home_screen.dart
 
+import 'package:celeb_voice/common/widgets/form_button.dart';
+import 'package:celeb_voice/constants/gaps.dart';
+import 'package:celeb_voice/constants/sizes.dart';
 import 'package:celeb_voice/features/main/models/celeb_models.dart';
 import 'package:celeb_voice/features/main/views_models/celeb_data.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +14,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
     List<CelebModel> celebs = CelebData.getCelebs();
     return Scaffold(
       appBar: AppBar(
@@ -31,12 +36,13 @@ class HomeScreen extends StatelessWidget {
             Container(
               padding: EdgeInsets.symmetric(vertical: 30),
               child: SizedBox(
-                height: 1000, // 빨간색(500) + 파란색(500)
+                height: screenHeight * 0.78, // 전체화면에서 78% 높이
                 child: PageView.builder(
                   controller: PageController(
                     viewportFraction: 0.85,
                     initialPage: 10000, // 큰 숫자로 시작해서 양방향 무한 스크롤
                   ),
+                  clipBehavior: Clip.none, // ← 이거 추가해보세요!
                   itemBuilder: (context, index) {
                     final celebIndex = index % celebs.length; // 실제 데이터 인덱스
                     return Padding(
@@ -45,25 +51,106 @@ class HomeScreen extends StatelessWidget {
                         children: [
                           // 빨간색 카드
                           Container(
-                            height: 500,
+                            clipBehavior: Clip.none,
+                            height: screenHeight * 0.5,
                             decoration: BoxDecoration(
-                              color: Colors.red,
+                              color: Colors.white,
                               borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: Center(
-                              child: Text(
-                                '빨간 카드 $celebIndex',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.1),
+                                  blurRadius: 10,
+                                  offset: Offset(0, 4),
                                 ),
-                              ),
+                              ],
+                            ),
+                            child: Stack(
+                              clipBehavior: Clip.none,
+                              children: [
+                                // 이미지
+                                Align(
+                                  child: ShaderMask(
+                                    shaderCallback: (Rect bounds) {
+                                      return LinearGradient(
+                                        begin: Alignment.topCenter,
+                                        end: Alignment.bottomCenter,
+                                        colors: [
+                                          Colors.white,
+                                          Colors.white,
+                                          Colors.transparent,
+                                        ],
+                                        stops: [0.0, 0.65, 0.95],
+                                      ).createShader(bounds);
+                                    },
+                                    blendMode: BlendMode.dstIn,
+                                    child: Transform.translate(
+                                      offset: Offset(0, -30),
+                                      child: FractionallySizedBox(
+                                        widthFactor: 1,
+                                        heightFactor: 1,
+                                        child: Image.asset(
+                                          celebs[celebIndex].imagePath,
+                                          fit: BoxFit.contain,
+                                          alignment: Alignment.bottomRight,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                // 이름과 태그
+                                Align(
+                                  alignment: Alignment.bottomLeft,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(
+                                      bottom: 100,
+                                      left: Sizes.size20,
+                                    ),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize
+                                          .min, // Column이 필요한 만큼만 공간 차지
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start, // 왼쪽 정렬
+                                      children: [
+                                        Text(
+                                          celebs[celebIndex].name,
+                                          style: TextStyle(
+                                            fontSize: 24,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Gaps.v8,
+                                        Text(
+                                          celebs[celebIndex].tags.toString(),
+                                          style: TextStyle(
+                                            fontSize: 14,
+                                            color: Colors.grey[600],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                // 구독하기 버튼
+                                GestureDetector(
+                                  onTap: () {},
+                                  child: Align(
+                                    alignment: Alignment.bottomCenter,
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                        horizontal: Sizes.size20,
+                                        vertical: 20,
+                                      ),
+                                      child: FormButton(text: '구독하기'),
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ),
                           // 파란색 카드 (연동)
+                          Gaps.v24,
                           Container(
-                            height: 500,
+                            height: screenHeight * 0.2,
                             decoration: BoxDecoration(
                               color: Colors.blue,
                               borderRadius: BorderRadius.circular(12),
