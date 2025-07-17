d5aaba0c5cbef93702adfae5cb24ac63a80da4d5
황광회 <ghl92479@gmail.com>
2025-07-17 09:35:00 +0900

    ADD: 미리듣기

--- lib/features/generation/views/preview_tts_screen.dart
+++ lib/features/generation/views/preview_tts_screen.dart
@@ -47,7 +47,80 @@ class _PreviewTtsScreenState extends State<PreviewTtsScreen> {
           icon: const Icon(Icons.arrow_back),
         ),
       ),
-      body: const Center(child: Text("PreviewTtsScreen")),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 10,
+                right: 10,
+                top: 20,
+                bottom: 10,
+              ),
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 2),
+                decoration: BoxDecoration(
+                  color: Color(0xff9e9ef4),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  "2025년 07월 16일 수요일",
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            // 메세지 박스 추가
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 16,
+                right: 16,
+                top: 5, // 상단 padding 줄임
+                bottom: 10,
+              ),
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 8,
+                      offset: Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "민지야, 누구나 누군가의 위로가 되어줄 수 있어.",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff463e8d),
+                      ),
+                    ),
+                    Text(
+                      "내 목소리로 조금이나마 행복했으면 좋겠다. 네가 웃으면 나도 좋아",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
